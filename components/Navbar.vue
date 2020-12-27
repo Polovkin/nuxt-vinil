f48eb388cb9b328f9ab1174e95c0f827066736d4
Polovkin <zimboroda@gmail.com>
2020-12-27 23:23:31 +0200

    feat(test): vuex-module-decorator
    Add simple action call

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,6 +5,7 @@
       <a href="/">{{ }}</a>
       <a href="/about">About</a>
       <a href="/users">Users</a>
+      <button @click="click">Press</button>
     </div>
   </header>
 </template>
@@ -12,19 +13,27 @@
 
 <script lang="ts">
 import {Vue, Component} from 'vue-property-decorator'
-import { getModule } from 'vuex-module-decorators'
+import {getModule} from 'vuex-module-decorators'
 import Test from '~/store/test'
 
 @Component({
-  mounted (this: Navbar) {
-    const test = getModule(Test, this.$store)
-    console.log(test.count) // undefined
-    console.log(this.$store.state['test'].count) // 42
-  }
+
 })
 export default class Navbar extends Vue {
   public header: string = 'value'
 
+  mounted() {
+
+
+
+    // console.log(this.$store.state['test'].count) // 42
+  }
+
+  click() {
+    const test = getModule(Test, this.$store)
+    test.incr()
+    console.log(test.count) // undefined
+  }
 
   get heading(): string {
     return this.header + '1'
