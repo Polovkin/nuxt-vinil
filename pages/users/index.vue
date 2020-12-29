555b06fa5e79ace28c130de0c44844d65d8d839a
Polovkin <zimboroda@gmail.com>
2020-12-29 22:14:31 +0200

    fix(int): try add lint

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,7 +3,7 @@
     <h1>Users page</h1>
     <ul>
       <li v-for="user of usersData"
-                   :key="user.id">
+          :key="user.id">
         <button @click="openUser(user.id)"> {{ user.name }}</button>
       </li>
     </ul>
@@ -11,24 +11,35 @@
 
 </template>
 
-<script>
+<script lang="ts">
 import {Vue, Component} from 'nuxt-property-decorator'
 import {usersModule} from '~/store'
 
 
 @Component({})
 export default class Users extends Vue {
+
   async fetch({store}) {
     if (store.getters['users/GET_USERS'].length === 0) {
       await store.dispatch('users/getUsers')
     }
   }
+
   get usersData() {
     return usersModule.users
   }
 
-  async created() {
+  async mounted() {
     await usersModule.getUsers()
+
+    function polova(n) {
+      return n * n
+    }
+
+    let a = 1
+    let b = 'z'
+    console.log(a * b);
+    polova('z')
   }
 
   openUser(user) {
