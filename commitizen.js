49512a4c211a0c2ad31b436996cdc8b97eae640f
Polovkin <zimboroda@gmail.com>
2021-01-02 21:12:55 +0200

    ci(comitizen): add new types

--- commitizen.js
+++ commitizen.js
@@ -1,18 +1,34 @@
-'use strict';
+'use strict'
 
 module.exports = {
-    types: [
-        {
-            value: 'feat',
-            name: 'feat:      A new feature',
-        },
-        {
-            value: 'fix',
-            name: 'fix:       A bug fix',
-        },
-    ],
-    allowCustomScopes: true,
-    allowBreakingChanges: false,
-    footerPrefix: 'METADATA:',
-    subjectLimit: 72,
-};
+  types: [
+    {
+      value: 'feat',
+      name: 'feat:      A new feature'
+    },
+    {
+      value: 'fix',
+      name: 'fix:       A bug fix'
+    },
+    {
+      value: 'style',
+      name: 'style:       A style change'
+    },
+    {
+      value: 'refactor',
+      name: 'refactor:       A bug fix,no feature add'
+    },
+    {
+      value: 'perf',
+      name: 'perf:       A code change that improves performance'
+    },
+    {
+      value: 'ci',
+      name: 'ci:       Change ci config'
+    }
+  ],
+  allowCustomScopes: true,
+  allowBreakingChanges: false,
+  footerPrefix: 'METADATA:',
+  subjectLimit: 72
+}
